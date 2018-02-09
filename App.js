f134d0c1bc83635ca609c6a43d89dc92585bde66
Radulescu Petru <petruradulescu.v@gmail.com>
2018-02-09 22:29:48 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <Text>ptr</Text>
+    )
+  }
+}
+
+export default HelloWorld;
+
+
+
